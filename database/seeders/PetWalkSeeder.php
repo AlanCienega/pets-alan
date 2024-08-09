9d5e2835aa39393fd63739521543f6eca0e543be
AlanCienega <craulalan115@gmail.com>
2024-08-09 17:18:22 -0600

    pet walks resources

---
+++ database/seeders/PetWalkSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PetWalkSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
